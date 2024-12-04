0a544de4dda25cf552bc341abc5772e872020dee
Hemang Mehra <mehra.hemang2023@gmail.com>
2024-12-04 13:15:38 -0800

    .

--- Assets/StayInRoom.cs
+++ Assets/StayInRoom.cs
@@ -2,16 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StayInRoom : MonoBehaviour
 {
     private Transform t;
     private Vector3 pos;
+    private Quaternion dir;
 
     private void Start()
     {
         t = GetComponent<Transform>();
         pos = t.position;
+        dir = t.rotation;
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -20,6 +24,7 @@ public class StayInRoom : MonoBehaviour
         {
             Debug.Log(other.name);
             t.position = pos;
+            t.rotation = dir;
         }
     }
     
