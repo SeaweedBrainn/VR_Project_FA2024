8894f4f23284a91cff2115fabdf77963ad076f36
Hemang Mehra <mehra.hemang2023@gmail.com>
2024-12-04 14:00:49 -0800

    .

--- Assets/0ClickMe/Hemang/Assets/Scripts/SceneLoader.cs
+++ Assets/0ClickMe/Hemang/Assets/Scripts/SceneLoader.cs
@@ -7,6 +7,7 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadScene()
     {
-        SceneManager.LoadScene(sceneToLoad);
+        //Resources.UnloadUnusedAssets();
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
     }
 }
